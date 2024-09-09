3836275ea3281e0b53789572cb6831f0a0de50f7
AomSirawit <110830004+AomSirawit@users.noreply.github.com>
2024-09-09 17:52:09 +0700

    change navbar design

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
    <div>
-    <p className="text-center my-5 font-semibold text-sm">Sirawit <span className="text-primary">Portfolio</span></p>
+    <p className="text-center my-5 font-semibold text-sm">Sirawit <span className="text-primary">Portfolio</span> Copyright © All Right Reserved</p>
    </div>
   );
 }
