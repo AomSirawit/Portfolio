f0e6ace98424abd40b80cdd8368d80ff1dbdc1d0
AomSirawit <110830004+AomSirawit@users.noreply.github.com>
2024-09-10 09:43:58 +0700

    fix some responsive

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -6,7 +6,7 @@ import WebBoard from '../assets/webboard.png'
 function Project() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen" id="project">
+      <div className="flex items-center justify-center" id="project">
         <div className="text-center">
           <div className="m-5">
             <h1 className="text-6xl text-primary font-bold bg-gradient-to-r from-blue-600 to-indigo-400 inline-block text-transparent bg-clip-text outer-glow mb-5">
